5d8b8dd09d7f451fa0feec0193dafd6d34e3198d
sbgrb <974487599@qq.com>
2020-10-23 12:14:39 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,29 +1,22 @@
 const $siteList = $('.siteLink');
 const $lastList = $siteList.find('li.last');
-const x = localStorage.getItem('x')
-const xObject = JSON.parse(x)
+const x = localStorage.getItem('x');
+const xObject = JSON.parse(x);
 const hashMap = xObject || [
-    {
-        logo: "G",
-        url: "https://www.google.com/"
-    },
-    {
-        logo: "I",
-        url: "https://www.iconfont.cn/"
-    },
+    {logo: "G", url: "https://www.google.com/"},
+    {logo: "I", url: "https://www.iconfont.cn/"},
 ];
 const simplifyUrl = (url) => {
     return url.replace('https://', '')
         .replace('http://', '')
         .replace('www.', '')
-        .replace(/\/.*/, '')
-}
+        .replace(/\/.*/, '');
+};
 
 const render = () => {
-    $siteList.find('li:not(.last)').remove()
-    hashMap.forEach((node , index) => {
-        const $li = $(` 
-                <li>
+    $siteList.find('li:not(.last)').remove();
+    hashMap.forEach((node, index) => {
+        const $li = $(` <li>
                     <div class="site">
                     <div class="logo">${node.logo}</div>
                     <div class="link">${simplifyUrl(node.url)}</div>
@@ -33,40 +26,37 @@ const render = () => {
                           </svg>
                      </div>
                    </div>
-                </li>`).insertBefore($lastList)
-        $li.on('click',()=>{
-            window.open(node.url)
-        })
+                </li>`).insertBefore($lastList);
+        $li.on('click', () => {
+            window.open(node.url);
+        });
 
-        $li.on('click','.close',(e)=>{
-            e.stopPropagation()
-            hashMap.splice(index,1)
-            render()
-        })
-    })
-}
-render()
+        $li.on('click', '.close', (e) => {
+            e.stopPropagation();
+            hashMap.splice(index, 1);
+            render();
+        });
+    });
+};
+render();
 
-$('.addButton')
-    .on('click', () => {
-        let url = window.prompt(`请输入你需要添加的网址`)
-        if (url.indexOf('http' === 0)) {
-
-        }else{
-            url = "https://" + url
+$('.addButton').on('click', () => {
+        let url = window.prompt(`请输入你需要添加的网址`);
+        if(url.indexOf('http')!==0){
+            url = 'https://' + url
         }
         hashMap.push({
             logo: simplifyUrl(url)[0].toUpperCase(),
             logoType: 'text',
             url: url
         });
-        render()
+        render();
     });
 
 window.onbeforeunload = () => {
-    const string = JSON.stringify(hashMap)
-    localStorage.setItem('x', string)
-}
+    const string = JSON.stringify(hashMap);
+    localStorage.setItem('x', string);
+};
 // $(document).on('keypress',(e) =>{
 //     const {key}  = e
 //     for (let i = 0;i<hashMap.length;i++){
